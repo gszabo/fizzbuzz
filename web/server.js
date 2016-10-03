b0dfaa88f49afd71f3a63a600a096c397d8325b4
Gabor Szabo <gabor.szabo@emarsys.com>
2016-10-03 21:42:14 +0200

    fix port env

--- web/server.js
+++ web/server.js
@@ -2,7 +2,7 @@ const app = require('./endpoints');
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(3000, function() {
+const server = app.listen(PORT, function() {
   const address = server.address();
   console.log('app listening', address);
 });
